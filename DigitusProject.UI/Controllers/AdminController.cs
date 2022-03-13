5aef35298bc86d129f833273ac4078169e6a328b
Buğrahan <bsaygici@hotmail.com>
2022-03-13 23:14:08 +0300

    Update AdminController.cs

--- DigitusProject.UI/Controllers/AdminController.cs
+++ DigitusProject.UI/Controllers/AdminController.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using System.IO;
 using Microsoft.AspNetCore.Authorization;
+using DigitusProject.WebUI.Models;
 
 namespace DigitusProject.WebUI.Controllers
 {
     [Authorize(Roles ="admin")]
     public class AdminController : Controller
     {
+        public int NumberOfOnlineUsers()
+        {
+            int count = 0;
+            count = ClientSource.Clients.Count();
+            return count;
+        }
     }
 }
